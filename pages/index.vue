cd81d9b2f380f78f19c982d5148ee9ca87de3884
Hiratake <hiratake@hiratake.xyz>
2020-12-11 15:57:00 +0900

    IntroSectionを調整♻

--- pages/index.vue
+++ pages/index.vue
@@ -11,52 +11,62 @@
     </section>
     <!-- Intro section -->
     <section class="intro-section">
-      <div class="intro-section__main">
-        <div class="intro-section__text-container">
-          <h2 class="intro-section__catchcopy">
-            ここで爆発します、<br />あなたの可能性が。
-          </h2>
-          <div class="intro-section__text">
-            <p>jaoの世界へようこそ！</p>
-            <p>
-              このサーバには、やべーやつらが生息し、独特で最高にイカしたコミュニティをつくっています。
-            </p>
-          </div>
-          <app-button
-            color="discord"
-            href="https://discord.com/invite/YjwYEBk"
-            large
-          >
-            <app-icon icon="discord" left />
-            Discordに参加する
-          </app-button>
+      <div class="intro-section__container intro-section__container--left">
+        <h2 class="intro-section__catchcopy">
+          ここで爆発します、<br />あなたの可能性が。
+        </h2>
+        <div class="intro-section__text">
+          <p>jaoの世界へようこそ！</p>
+          <p>
+            このサーバには、やべーやつらが生息し、独特で最高にイカしたコミュニティをつくっています。
+          </p>
         </div>
-        <div class="intro-section__members-container">
-          <div class="intro-section__greeting">jao<br />afa</div>
-          <div class="intro-section__members intro-section__members--admin">
-            <template v-for="item in members.admin">
-              <nuxt-link
-                :key="item.id"
-                :to="'/community/users/' + item.uuid"
-                class="intro-section__user"
-              >
-                <img :src="item.icon" loading="lazy" />
-              </nuxt-link>
-            </template>
-          </div>
-          <div class="intro-section__members intro-section__members--moderator">
-            <template v-for="item in members.moderator">
-              <nuxt-link
-                :key="item.id"
-                :to="'/community/users/' + item.uuid"
-                class="intro-section__user"
-              >
-                <img :src="item.icon" loading="lazy" />
-              </nuxt-link>
-            </template>
-          </div>
-          <p>（jao鯖のゆかいな運営メンバーたち）</p>
+        <app-button
+          color="discord"
+          href="https://discord.com/invite/YjwYEBk"
+          large
+        >
+          <app-icon icon="discord" left />
+          Discordに参加する
+        </app-button>
+      </div>
+      <div class="intro-section__container intro-section__container--right">
+        <div class="intro-section__balloon">jao<br />afa</div>
+        <div class="intro-section__members intro-section__members--admin">
+          <template v-for="item in members.admin">
+            <nuxt-link
+              :key="item.id"
+              :to="'/community/users/' + item.uuid"
+              :title="item.name"
+              class="intro-section__user"
+            >
+              <img
+                :src="
+                  'https://crafatar.com/avatars/' + item.uuid + '?overlay=true'
+                "
+                loading="lazy"
+              />
+            </nuxt-link>
+          </template>
         </div>
+        <div class="intro-section__members intro-section__members--moderator">
+          <template v-for="item in members.moderator">
+            <nuxt-link
+              :key="item.id"
+              :to="'/community/users/' + item.uuid"
+              :title="item.name"
+              class="intro-section__user"
+            >
+              <img
+                :src="
+                  'https://crafatar.com/avatars/' + item.uuid + '?overlay=true'
+                "
+                loading="lazy"
+              />
+            </nuxt-link>
+          </template>
+        </div>
+        <p>（jao鯖のゆかいな運営メンバーたち）</p>
       </div>
     </section>
     <!-- Gallery section -->
@@ -79,29 +89,21 @@ export default {
           {
             id: 'X4Z',
             uuid: '5799296a-d1ec-4252-93bd-440bb9caa65c',
-            icon:
-              'https://crafatar.com/avatars/5799296a-d1ec-4252-93bd-440bb9caa65c',
             name: 'Zakuro',
           },
           {
             id: 'Hirotaisou2012',
             uuid: '39cf878b-ef0b-44fc-a2c6-de3d540a4728',
-            icon:
-              'https://crafatar.com/avatars/39cf878b-ef0b-44fc-a2c6-de3d540a4728?overlay=true',
             name: 'Hiratake',
           },
           {
             id: 'mine_book000',
             uuid: '32ff7cdc-a1b4-450a-aa7e-6af75fe8c37c',
-            icon:
-              'https://crafatar.com/avatars/32ff7cdc-a1b4-450a-aa7e-6af75fe8c37c?overlay=true',
             name: 'Tomachi',
           },
           {
             id: 'X9Z',
             uuid: '7008531a-539b-4dfc-8b81-7b267d18dd0a',
-            icon:
-              'https://crafatar.com/avatars/7008531a-539b-4dfc-8b81-7b267d18dd0a?overlay=true',
             name: 'Omelet',
           },
         ],
@@ -109,36 +111,26 @@ export default {
           {
             id: 'Ekusas83',
             uuid: '22ab15d6-2e88-4fdd-91c0-e459f0f804f8',
-            icon:
-              'https://crafatar.com/avatars/22ab15d6-2e88-4fdd-91c0-e459f0f804f8?overlay=true',
             name: 'Ekusas',
           },
           {
             id: 'X5Z',
             uuid: '0bdc0219-f3c3-4d73-a4df-1d8bd088f419',
-            icon:
-              'https://crafatar.com/avatars/0bdc0219-f3c3-4d73-a4df-1d8bd088f419?overlay=true',
             name: 'Zozokasu',
           },
           {
             id: 'kohonayoshi',
             uuid: '26728d53-add7-46d1-97c3-0a25bc6607f5',
-            icon:
-              'https://crafatar.com/avatars/26728d53-add7-46d1-97c3-0a25bc6607f5?overlay=true',
             name: 'Kohona',
           },
           {
             id: 'Nudonge',
             uuid: '22b69dc2-a8e7-4ffd-a3d0-54023d687d5f',
-            icon:
-              'https://crafatar.com/avatars/22b69dc2-a8e7-4ffd-a3d0-54023d687d5f?overlay=true',
             name: 'Nudonge',
           },
           {
             id: 'MinHero_exp',
             uuid: '0ad34a33-3ca4-4c86-84f3-a4591920b06a',
-            icon:
-              'https://crafatar.com/avatars/0ad34a33-3ca4-4c86-84f3-a4591920b06a?overlay=true',
             name: 'Ekipa',
           },
         ],
@@ -208,7 +200,7 @@ export default {
 }
 
 // Intro section
-.intro-section__main {
+.intro-section {
   display: flex;
   flex-wrap: wrap;
   align-items: center;
@@ -228,6 +220,31 @@ export default {
   }
 }
 
+.intro-section__container {
+  width: 100%;
+
+  @include bp(md) {
+    width: auto;
+  }
+
+  & ~ & {
+    margin-top: ($size-base * 8);
+
+    @include bp(md) {
+      margin-top: 0;
+    }
+  }
+
+  &--right {
+    display: grid;
+    grid-template-rows: repeat(4, auto);
+    grid-template-columns: 1fr;
+    gap: ($size-base * 2);
+    justify-items: center;
+    text-align: center;
+  }
+}
+
 .intro-section__catchcopy {
   font-size: $font-size-l3;
   line-height: 1.5;
@@ -241,14 +258,6 @@ export default {
   }
 }
 
-.intro-section__text-container {
-  width: 100%;
-
-  @include bp(md) {
-    width: auto;
-  }
-}
-
 .intro-section__text {
   max-width: ($size-base * 44);
   margin: ($size-base * 3) auto ($size-base * 4);
@@ -258,23 +267,7 @@ export default {
   }
 }
 
-.intro-section__members-container {
-  display: grid;
-  grid-template-rows: repeat(4, auto);
-  grid-template-columns: 1fr;
-  gap: ($size-base * 2);
-  justify-items: center;
-  width: 100%;
-  margin-top: ($size-base * 8);
-  text-align: center;
-
-  @include bp(md) {
-    width: auto;
-    margin-top: 0;
-  }
-}
-
-.intro-section__greeting {
+.intro-section__balloon {
   position: relative;
   width: ($size-base * 13);
   padding: ($size-base * 1) 0;
